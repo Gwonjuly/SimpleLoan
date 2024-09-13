67dd8ff44729c25893279aa0b0bfe5b4b306e648
Gwonjulee <wnfl0925@naver.com>
2024-09-13 18:50:59 +0900

    init

---
+++ src/main/java/com/example/SimpleLoan/SimpleLoanApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleLoan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleLoanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimpleLoan/SimpleLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleLoan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
